eb4791dfaf31956a9004601a3ebb5c9836853169
italo.andre2007 <italo.andre2007@gmail.com>
2018-12-09 13:08:08 -0300

    Deployed da aplicação

---
+++ aplication/src/views/NodesMap.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<p>Teste</p>
+	</div>
+</template>
+
+<script>
+	import axios from 'axios';
+
+	export default {
+
+		mounted() {
+			axios.get('http://localhost:8000/')
+			.then(function (response) {
+				console.log(respose);console.log(respose);
+			})
+			.catch(function (error) {
+			})
+		},
+	}
+</script>
+
+<style scoped>
+
+</style>
