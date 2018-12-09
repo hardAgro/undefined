38eda4907dbb36e83f4122c14c941a30f028e7f5
uesleymelo <uesley.eumesmo@gmail.com>
2018-12-09 16:31:02 -0300

    lista nodes com rps

--- aplication/src/main.js
+++ aplication/src/main.js
@@ -1,11 +1,14 @@
 /* eslint-disable */
 import Vue from 'vue';
 import ShardsVue from 'shards-vue';
+import BootstrapVue from 'bootstrap-vue'
 
 // Styles
 import 'bootstrap/dist/css/bootstrap.css';
 import '@/scss/shards-dashboards.scss';
 import '@/assets/scss/date-range.scss';
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 // Core
 import App from './App.vue';
@@ -17,6 +20,8 @@ import Default from '@/layouts/Default.vue';
 ShardsVue.install(Vue);
 
 Vue.component('default-layout', Default);
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 Vue.prototype.$eventHub = new Vue();
